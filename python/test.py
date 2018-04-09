638d1b7b0bf72c4639452b70fcb931c525055b87
nir0303 <nbniranjan38@gmail.com>
2018-04-08 22:54:27 -0400

    Test changes

--- python/test.py
+++ python/test.py
@@ -8,8 +8,8 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
 
-def reconstruct_image_4():
-    test_label = np.load("cache/test_predict2.npy")
+def reconstruct_image_3():
+    test_label = np.load("cache/test_predict2_3class.npy")
     for image_index in range(len(test_label)):
         image = test_label[image_index].transpose(1, 2, 0)
         for x, i in enumerate(image):
@@ -19,11 +19,7 @@ def reconstruct_image_4():
                 j1 = j[1]
                 j2 = j[2]
                 # print(j0,j1,j2)
-                if abs(j0-j1) < 0.005 and abs(j0-j1) < 0.005:
-                    image[x, y, 0] = 0
-                    image[x, y, 1] = 0
-                    image[x, y, 2] = 0
-                    continue
+
                 if j0 > j1 and j0 > j2:
                     image[x, y, 0] = 255
                 else:
@@ -45,11 +41,15 @@ def reconstruct_image_4():
         image.save('data/predict/label_{}.png'.format(image_index))
 # image.show()
 
-test_label = np.load("cache/test_predict2.npy")
-for i in range(len(test_label)):
-    image_data = test_label[i]
-    image_data = np.uint8(np.where(image_data>= 0.1, 255, 0)).reshape(565, 565)
-    image = Image.fromarray(image_data, mode='L')
-    image.save('data/predict/1_class/label_{}.png'.format(i+1))
+def reconstruct_1class():
+    test_label = np.load("cache/test_predict2.npy")
+    for i in range(len(test_label)):
+        print("yes")
+        image_data = test_label[i]
+        image_data = np.uint8(np.where(image_data>= 0.00001, 255, 0)).reshape(565, 565)
+        image = Image.fromarray(image_data, mode='L')
+        image.save('data/predict/1_class/label_{}.png'.format(i+1))
 
 
+# reconstruct_image_3()
+reconstruct_1class()
