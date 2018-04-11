cd2aebd3b4978086ba60c448c74cc1395dd75fb3
nir0303 <nbniranjan38@gmail.com>
2018-04-11 15:44:41 -0400

    reconstruct vectorized approach

--- python/reconstruct_image.py
+++ python/reconstruct_image.py
@@ -9,54 +9,28 @@ def softmax(x):
     return e_x / e_x.sum(axis=0)
 
 
-def reconstruct_image(classification='4_class', data=None):
+def reconstruct_image_vec(classification='4_class', data=None):
     if not data:
         return
     predict_label = np.load(data)
     for image_index in range(len(predict_label)):
         image_data = predict_label[image_index]
         image_data = softmax(expit(image_data)).transpose(1, 2, 0)
-        # image_data = softmax(expit(image_data))
-        for x, i in enumerate(image_data):
-            for y, j in enumerate(i):
-                j = softmax(expit(j))
-                if classification == '2_class':
-                    j0 = j[0]
-                    j1 = j[1]
-                    if j0 > j1:
-                        image_data[x, y, 0] = 255
-                    else:
-                        image_data[x, y, 0] = 0
-                elif classification == '4_class':
-                    j0 = j[0]
-                    j1 = j[1]
-                    j2 = j[2]
-                    j3 = j[3]
-                    # print(j0, j1, j2, j3)
-                    if j0 >= j1 and j0 >= j2 and j0 >= j3:
-                        image_data[x, y, 0] = 255
-                    else:
-                        image_data[x, y, 0] = 0
-                    if j1 >= j0 and j1 >= j2 and j1 >= j3:
-                        image_data[x, y, 2] = 255
-                    else:
-                        image_data[x, y, 2] = 0
-                    if j2 >= j1 and j2 >= j1 and j2 >= j3:
-                        image_data[x, y, 1] = 255
-                    else:
-                        image_data[x, y, 1] = 0
-                    if j3 >= j0 and j3 >= j2 and j3 >= j1:
-                        image_data[x, y, 0] = 0
-                        image_data[x, y, 1] = 0
-                        image_data[x, y, 2] = 0
-                    image_data = image_data[..., 0:3]
-            
-            if classification == '2_class':
-                image = Image.fromarray(np.uint8(image_data[..., 0]).reshape(565, 565), mode='L')
-            elif classification == '4_class':
-                image = Image.fromarray(np.uint8(image_data), mode='RGB')
-            image.save('data/predict/{}/label_{}.png'.format(classification,image_index))
+        image_data_max = np.max(image_data, axis=2)
+        background = image_data_max == image_data[..., -1]
+        if classification == '4_class':
+            a = image_data_max == image_data[..., 0]
+            o = image_data_max == image_data[..., 1]
+            v = image_data_max == image_data[..., 2]
+            image_r = np.where(np.stack((a, o, v), axis=2), 255, 0)
+            image = Image.fromarray(np.uint8(image_r), mode='RGB') 
+        elif classification == '2_class':
+            optic_nerve = image_data_max == image_data[..., 0]
+            image_r = np.where(optic_nerve, 255, 0).reshape(565, 565)
+            image = Image.fromarray(np.uint8(image_r), mode='L')
+    # image.show()
+        image.save('data/predict/{}/label_{}.png'.format(classification, image_index))
 
 
-# reconstruct_image(classification='2_class', data="cache/test_predict2_class_2.npy")
-reconstruct_image(classification='4_class', data="cache/test_predict2_class_4_1.npy")
+reconstruct_image_vec(classification='2_class', data="cache/test_predict2_class.npy")
+reconstruct_image_vec(classification='4_class', data="cache/test_predict2_class_4.npy")
