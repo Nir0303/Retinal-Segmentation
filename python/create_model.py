added softmax entropy back and accuracy fix

python/create_model.py
@@ -28,11 +28,9 @@ cur_dir = os.getcwd()
 
 def image_accuracy(y_true, y_pred):
     with tf.name_scope("ImageAccuracy"):
-        # print(y_pred.shape)
-        # X_sigmoid = tf.nn.sigmoid(y_true, name="Sigmoid")
-        # X_softmax = tf.nn.softmax(X_sigmoid, axis=1, name="Softmax")
-        y_pred = y_pred[:, :-1, :, :]
-        verify = tf.cast(tf.equal(tf.argmax(y_true, axis=1),
+        X_sigmoid = tf.nn.sigmoid(y_true, name="Sigmoid")
+        X_softmax = tf.nn.softmax(X_sigmoid, axis=1, name="Softmax")
+        verify = tf.cast(tf.equal(tf.argmax(X_softmax, axis=1),
                                   tf.argmax(y_pred, axis=1), name="Compare"),
                          dtype=tf.float32, name="Cast")
         accuracy = tf.reduce_mean(verify, name="Accuracy")
@@ -46,6 +44,13 @@ def sigmoid_cross_entropy_with_logits(target, output):
                                                    logits=output, name="SigmoidCrossEntropy")
         return tf.reduce_mean(loss, axis=1, name="LossMean")
 
+def softmax_cross_entropy_with_logits(target, output):
+    with tf.name_scope ("SigmoidCrossEntropyLoss"):
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=target,
+                                                          logits=output,
+                                                          dim=1
+                                                          )
+        return tf.reduce_mean(loss, axis=-1)
 
 def parse_args():
     """

python/tf_conv.py
@@ -5,16 +5,17 @@ from PIL import Image
 
 def tf_accuracy():
     test_label = prepare_image.load_images(data_type="test", image_type="label", classification=4,
-                                                 dataset="small")
-    predict_label = np.load('cache/test_predict2_class_4.npy')
-    X = tf.placeholder(dtype=tf.float32, shape=test_label.shape)
-    Y = tf.placeholder(dtype=tf.float32, shape=test_label.shape)
+                                                dataset="small")
+    predict_label = np.load('cache/test_predict2_class_4_reg.npy')
+    X = tf.constant(predict_label,dtype=tf.float32, shape=test_label.shape)
+    Y = tf.constant(test_label, dtype=tf.float32, shape=test_label.shape)
     X_sigmoid = tf.nn.sigmoid(X)
-    X_softmax = tf.nn.softmax(X_sigmoid, axis=0)
+    X_softmax = tf.nn.softmax(X_sigmoid, axis=1)
     sess = tf.InteractiveSession()
+
     verify = tf.cast(tf.equal(tf.argmax(X_softmax, axis=1), tf.argmax(Y, axis=1)),dtype=tf.float32)
     accuracy = tf.reduce_mean(verify)
-    print(sess.run(accuracy, feed_dict={X: predict_label, Y: test_label}))
+    print(sess.run(accuracy))
     
 def image_reconstruct():
     predict_label = np.load('cache/test_predict2_class_4.npy')