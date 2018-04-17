be438a2e8d70396faa7323e41c25141da66e39ea
nir0303 <nbniranjan38@gmail.com>
2018-04-16 20:22:15 -0400

    added dev model with more kernels

--- python/create_model_dev.py
+++ python/create_model_dev.py
@@ -44,13 +44,15 @@ def sigmoid_cross_entropy_with_logits(target, output):
                                                    logits=output, name="SigmoidCrossEntropy")
         return tf.reduce_mean(loss, axis=1, name="LossMean")
 
+
 def softmax_cross_entropy_with_logits(target, output):
-    with tf.name_scope ("SigmoidCrossEntropyLoss"):
+    with tf.name_scope("SigmoidCrossEntropyLoss"):
         loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=target,
                                                           logits=output,
                                                           dim=1
                                                           )
         return tf.reduce_mean(loss, axis=-1)
+
 
 def parse_args():
     """
@@ -130,6 +132,25 @@ class RetinaModel(object):
                           padding="SAME")(conv4_2)
         conv4_3 = Dropout(0.2, name="Drop4_3")(conv4_3)
 
+        conv5_1 = Conv2D(64, kernel_size=(5, 5), activation=self.activation, name="conv5_1",
+                          padding="SAME")(max_pool1)
+        conv5_1 = Dropout(0.2, name="Drop5_1")(conv5_1)
+        conv5_2 = Conv2D(64, kernel_size=(5, 5), activation=self.activation, name="conv5_2",
+                          padding="SAME")(conv5_1)
+        conv5_2 = Dropout(0.2, name="Drop5_2")(conv5_2)
+        max_pool4 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='max_pool4',
+                                  padding="SAME")(conv5_2)
+
+        # Convolution Layer 2
+        conv6_1 = Conv2D(128, kernel_size=(5, 5), activation=self.activation, name="conv6_1",
+                          padding="SAME")(max_pool4)
+        conv6_1 = Dropout(0.2, name="Drop6_1")(conv6_1)
+        conv6_2 = Conv2D(128, kernel_size=(3, 3), activation=self.activation, name="conv6_2",
+                          padding="SAME")(conv6_1)
+        conv6_2 = Dropout(0.2, name="Drop6_2")(conv6_2)
+        max_pool5 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='max_pool5',
+                                  padding="SAME")(conv6_2)
+
         #
         conv1_2_16 = Conv2D(16, kernel_size=(3, 3), name="conv1_2_16",
                              padding="SAME")(conv1_2)
@@ -144,6 +165,14 @@ class RetinaModel(object):
                              padding="SAME")(conv4_3)
         conv4_3_16 = Dropout(0.2, name="Drop4_3_16")(conv4_3_16)
 
+        conv5_2_16 = Conv2D(16, kernel_size=(3, 3), name="conv5_2_16",
+                             padding="SAME")(conv5_2)
+        conv5_2_16 = Dropout(0.2, name="Drop5_2_16")(conv5_2_16)
+        conv6_2_16 = Conv2D(16, kernel_size=(3, 3), name="conv6_2_16",
+                             padding="SAME")(conv6_2)
+        conv6_2_16 = Dropout(0.2, name="Drop6_2_16")(conv6_2_16)
+        
+
         # Deconvolution Layer1
         side_multi2_up = UpSampling2D(size=(2, 2), name="side_multi2_up")(conv2_2_16)
 
@@ -157,26 +186,36 @@ class RetinaModel(object):
         side_multi4_up = UpSampling2D(size=(8, 8), name="side_multi4_up")(conv4_3_16)
         upside_multi4 = Cropping2D(cropping=((1, 2),(1, 2)), name="upside_multi4")(side_multi4_up)
 
+        # Deconvolution Layer4
+        side_multi5_up = UpSampling2D(size=(2, 2), name="side_multi5_up")(conv5_2_16)
+
+        upside_multi5 = Cropping2D(cropping=((0, 1),(0, 1)), name="upside_multi5")(side_multi5_up)
+
+        # Deconvolution Layer1
+        side_multi6_up = UpSampling2D(size=(4, 4), name="side_multi6_up")(conv6_2_16)
+
+        upside_multi6 = Cropping2D(cropping=((1, 2),(1, 2)), name="upside_multi6")(side_multi6_up)
+
         # Specialized Layer
-        concat_upscore = concatenate([conv1_2_16, upside_multi2, upside_multi3, upside_multi4],
+        concat_upscore = concatenate([conv1_2_16, upside_multi2, upside_multi3, upside_multi4, 
+                                      upside_multi5, upside_multi6],
                                       name="concat-upscore", axis=1)
         upscore_fuse = Conv2D(self._classification, kernel_size=(1, 1), name="upscore_fuse")(concat_upscore)
         upscore_fuse = Dropout(0.2, name="Dropout_Classifier")(upscore_fuse)
 
         self.model = Model(inputs=[data_input], outputs=[upscore_fuse])
 
+
     def set_weights(self):
         if args.cache and os.path.exists("cache/keras_crop_model_weights_4class_reg.h5"):
-            print("yes")
-            self.model.load_weights("cache/keras_crop_model_weights_4class_reg.h5")
-            return
-            with open("cache/2_class_model.json") as f:
-                model_2class = model_from_json(json.dumps(json.load(f)))
-            model_2class.load_weights("cache/keras_crop_model_weights_2class_2.h5")
 
-            for layer, layer2 in zip(self.model.layers, model_2class.layers):
-                if(layer.name != 'upscore_fuse' and self._classification !=2) or self._classification == 2:
-                    layer.set_weights(layer2.get_weights())
+            with open("cache/4_class_model.json") as f:
+                model_4class = model_from_json(json.dumps(json.load(f)))
+            model_4class.load_weights("cache/keras_crop_model_weights_4class_reg.h5")
+
+            for layer, layer4 in zip(self.model.layers, model_4class.layers):
+                if layer.name == layer4.name:
+                    layer.set_weights(layer4.get_weights())
 
     def _write_hdf5(self, name, data):
         output_file = os.path.join(self.cache_image, name+'.h5')
@@ -237,7 +276,7 @@ class RetinaModel(object):
         self.model.fit(self.train_images, self.train_labels, batch_size=5, epochs=2000,
                         callbacks=[tb_callback], validation_split=0.05, verbose=1)
 
-        self.model.save_weights(os.path.join('cache', 'keras_crop_model_weights_4class_reg_{}.h5'.format(self.activation)))
+        self.model.save_weights(os.path.join('cache', 'keras_crop_model_weights_4class_dev_reg_{}.h5'.format(self.activation)))
 
     def predict(self):
         test_predict = self.model.predict(self.test_images, batch_size=10)
@@ -253,14 +292,18 @@ if __name__ == '__main__':
     rm = RetinaModel(classification=args.classification, dataset=args.dataset,
                      reload=args.reload, activation=args.activation)
     rm.create_model()
+
     rm.set_weights()
+
     rm.get_data()
     print(rm.test_labels.shape)
     print(rm.train_images.shape)
-    # print(rm.model.layers[1].get_weights())
-    # print(rm.model.layers[1].output_shape)
+    print(rm.model.layers[1].get_weights())
+    print(rm.model.layers[1].output_shape)
     # plot_model(rm.model,"model.png")
     rm.run()
     rm.predict()
-    # print(rm.model.summary())
+
+    # plot_model(rm.model, "model.png")
+    print(rm.model.summary())
     K.clear_session()
