3122365924e1aaef875867c67f33874c442aff20
nir0303 <nbniranjan38@gmail.com>
2018-04-18 09:45:38 -0400

    dev changes for encapsulation and polymorism

---
+++ python/base_model.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+import sys
+import os
+import numpy as np
+import tensorflow as tf
+import json
+import prepare_image
+import utility
+import h5py
+import keras
+
+from keras.models import Sequential, Model, model_from_json
+from keras.layers import Dense, Flatten, Dropout, Input, concatenate, merge, Add, Dropout
+from keras.layers import Conv2D, Conv2DTranspose, Cropping2D, ZeroPadding2D, Activation
+from keras.layers import MaxPooling2D, UpSampling2D, Permute
+from keras import backend as K
+from keras.activations import softmax
+import keras.backend.tensorflow_backend as tfb
+from keras.utils import plot_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.optimizers import SGD,Adam
+from time import time
+
+K.set_image_data_format("channels_first")
+cur_dir = os.getcwd()
+pylon5 = os.environ["SCRATCH"] if os.environ.get("SCRATCH", None) else "."
+pylon5_cache = os.path.join(pylon5, 'cache')
+
+def image_accuracy(y_true, y_pred):
+    with tf.name_scope("ImageAccuracy"):
+        X_sigmoid = tf.nn.sigmoid(y_true, name="Sigmoid")
+        X_softmax = tf.nn.softmax(X_sigmoid, axis=1, name="Softmax")
+        verify = tf.cast(tf.equal(tf.argmax(X_softmax, axis=1),
+                                  tf.argmax(y_pred, axis=1), name="Compare"),
+                         dtype=tf.float32, name="Cast")
+        accuracy = tf.reduce_mean(verify, name="Accuracy")
+        return accuracy
+
+
+def sigmoid_cross_entropy_with_logits(target, output):
+    with tf.name_scope("SigmoidCrossEntropyLoss"):
+        # print(target.get_shape())
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=target,
+                                                   logits=output, name="SigmoidCrossEntropy")
+        return tf.reduce_mean(loss, axis=1, name="LossMean")
+
+def softmax_cross_entropy_with_logits(target, output):
+    with tf.name_scope ("SigmoidCrossEntropyLoss"):
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=target,
+                                                          logits=output,
+                                                          dim=1
+                                                          )
+        return tf.reduce_mean(loss, axis=-1)
+
+
+class BaseModel(object):
+    def __init__(self, classification=3, dataset="big", reload="False", activation='relu', cache=True):
+        self.model = None
+        self.input = None
+        self.output = None
+        self._classification = classification
+        self.dataset = dataset
+        self.reload = reload
+        self.cache = cache
+        self.activation = activation if activation != 'leaky_relu' else tf.nn.leaky_relu
+
+    def create_model(self):
+        return Exception("Not Implemented Error")
+
+    def set_weights(self):
+        return Exception ("Not Implemented Error")
+
+    def _write_hdf5(self, name, data):
+        output_file = os.path.join(self.cache_image, name+'.h5')
+        with h5py.File(output_file, "w") as f:
+            f.create_dataset('image', data=data, dtype=data.dtype)
+
+    @staticmethod
+    def _load_hdf5(input_file):
+        with h5py.File(input_file, "r") as f:  # "with" close the file after its nested commands
+            return f["image"][()]
+
+    def get_data(self):
+        self.cache_image = os.path.join(pylon5_cache, self.dataset, 'image')
+        if self.reload:
+            utility.remove_directory(self.cache_image)
+        if self.cache and os.path.exists(self.cache_image):
+            # print(self.cache_image);exit()
+            self.train_images = self._load_hdf5(os.path.join(self.cache_image, 'train_images.h5'))
+            self.train_labels = self._load_hdf5(os.path.join(self.cache_image, 'train_labels.h5'))
+            self.test_images = self._load_hdf5(os.path.join(self.cache_image, 'test_images.h5'))
+            self.test_labels = self._load_hdf5(os.path.join(self.cache_image, 'test_labels.h5'))
+            return
+
+        self.train_images = prepare_image.load_images(data_type="train", image_type="image",
+                                                      classification=self._classification,
+                                                      dataset = self.dataset)
+        self.train_labels = prepare_image.load_images(data_type="train", image_type="label",
+                                                      classification=self._classification,
+                                                      dataset=self.dataset)
+        self.test_images = prepare_image.load_images(data_type="test", image_type="image",
+                                                     classification=self._classification,
+                                                     dataset=self.dataset)
+        self.test_labels = prepare_image.load_images(data_type="test", image_type="label",
+                                                     classification=self._classification,
+                                                     dataset=self.dataset)
+
+        if self.cache and not os.path.exists(self.cache_image):
+            utility.create_directory(self.cache_image)
+            self._write_hdf5('train_images', self.train_images)
+            self._write_hdf5('train_labels', self.train_labels)
+            self._write_hdf5('test_images', self.test_images)
+            self._write_hdf5('test_labels', self.test_labels)
+
+    def run(self):
+        return Exception("Not Implemented Error")
+
+
+    def predict(self):
+        return Exception ("Not Implemented Error")
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+    rm = RetinaModel(classification=args.classification, dataset=args.dataset,
+                     reload=args.reload, activation=args.activation, cache=args.cache)
+    rm.create_model()
+    rm.set_weights()
+    rm.get_data()
+    print(rm.test_labels.shape)
+    print(rm.train_images.shape)
+    # print(rm.model.layers[1].get_weights())
+    # print(rm.model.layers[1].output_shape)
+    # plot_model(rm.model,"model.png")
+    # rm.run()
+    # rm.predict()
+    K.clear_session()
--- python/main_model.py
+++ python/main_model.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
+
 import sys
 import os
 import argparse
 import numpy as np
 import tensorflow as tf
 import json
-import prepare_image
-import utility
-import h5py
 import keras
 
 from keras.models import Sequential, Model, model_from_json
@@ -21,36 +19,10 @@ from keras.utils import plot_model
 from keras.preprocessing.sequence import pad_sequences
 from keras.optimizers import SGD,Adam
 from time import time
-
-K.set_image_data_format("channels_first")
-cur_dir = os.getcwd()
-
-
-def image_accuracy(y_true, y_pred):
-    with tf.name_scope("ImageAccuracy"):
-        X_sigmoid = tf.nn.sigmoid(y_true, name="Sigmoid")
-        X_softmax = tf.nn.softmax(X_sigmoid, axis=1, name="Softmax")
-        verify = tf.cast(tf.equal(tf.argmax(X_softmax, axis=1),
-                                  tf.argmax(y_pred, axis=1), name="Compare"),
-                         dtype=tf.float32, name="Cast")
-        accuracy = tf.reduce_mean(verify, name="Accuracy")
-        return accuracy
+from base_model import(BaseModel,sigmoid_cross_entropy_with_logits,
+                        image_accuracy,softmax_cross_entropy_with_logits)
 
 
-def sigmoid_cross_entropy_with_logits(target, output):
-    with tf.name_scope("SigmoidCrossEntropyLoss"):
-        # print(target.get_shape())
-        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=target,
-                                                   logits=output, name="SigmoidCrossEntropy")
-        return tf.reduce_mean(loss, axis=1, name="LossMean")
-
-def softmax_cross_entropy_with_logits(target, output):
-    with tf.name_scope ("SigmoidCrossEntropyLoss"):
-        loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=target,
-                                                          logits=output,
-                                                          dim=1
-                                                          )
-        return tf.reduce_mean(loss, axis=-1)
 
 def parse_args():
     """
@@ -72,19 +44,13 @@ def parse_args():
     return args
 
 
-class RetinaModel(object):
-    def __init__(self, classification=3, dataset="big", reload="False", activation='relu'):
-        self.model = None
-        self.input = None
-        self.output = None
-        self._classification = classification
-        self.dataset = dataset
-        self.reload = reload
-        self.activation = activation if activation != 'leaky_relu' else tf.nn.leaky_relu
+class RetinaModel(BaseModel):
+    def __init__(self, classification=3, dataset="big", reload=False, activation='relu', cache=True):
+        super(RetinaModel, self).__init__(classification, dataset, reload, activation, cache)
 
     def create_model(self):
         print(self.activation)
-        input_shape =(3, 565, 565)
+        input_shape = (3, 565, 565)
 
         data_input = Input(shape=input_shape, name="data_input")
         conv1_1 = Conv2D(64, kernel_size=(3, 3), activation=self.activation, name="conv1_1",
@@ -166,60 +132,10 @@ class RetinaModel(object):
         self.model = Model(inputs=[data_input], outputs=[upscore_fuse])
 
     def set_weights(self):
-        if args.cache and os.path.exists("cache/keras_crop_model_weights_4class_reg.h5"):
+        if self.cache and os.path.exists("cache/keras_crop_model_weights_4class_reg.h5"):
             print("yes")
             self.model.load_weights("cache/keras_crop_model_weights_4class_reg.h5")
-            return
-            with open("cache/2_class_model.json") as f:
-                model_2class = model_from_json(json.dumps(json.load(f)))
-            model_2class.load_weights("cache/keras_crop_model_weights_2class_2.h5")
-
-            for layer, layer2 in zip(self.model.layers, model_2class.layers):
-                if(layer.name != 'upscore_fuse' and self._classification !=2) or self._classification == 2:
-                    layer.set_weights(layer2.get_weights())
-
-    def _write_hdf5(self, name, data):
-        output_file = os.path.join(self.cache_image, name+'.h5')
-        with h5py.File(output_file, "w") as f:
-            f.create_dataset('image', data=data, dtype=data.dtype)
-
-    @staticmethod
-    def _load_hdf5(input_file):
-        with h5py.File(input_file, "r") as f:  # "with" close the file after its nested commands
-            return f["image"][()]
-
-    def get_data(self):
-        self.cache_image = os.path.join(pylon5_cache, self.dataset, 'image')
-        if self.reload:
-            utility.remove_directory(self.cache_image)
-        if args.cache and os.path.exists(self.cache_image):
-            # print(self.cache_image);exit()
-            self.train_images = self._load_hdf5(os.path.join(self.cache_image, 'train_images.h5'))
-            self.train_labels = self._load_hdf5(os.path.join(self.cache_image, 'train_labels.h5'))
-            self.test_images = self._load_hdf5(os.path.join(self.cache_image, 'test_images.h5'))
-            self.test_labels = self._load_hdf5(os.path.join(self.cache_image, 'test_labels.h5'))
-            return
-
-        self.train_images = prepare_image.load_images(data_type="train", image_type="image",
-                                                      classification=self._classification,
-                                                      dataset = self.dataset)
-        self.train_labels = prepare_image.load_images(data_type="train", image_type="label",
-                                                      classification=self._classification,
-                                                      dataset=self.dataset)
-        self.test_images = prepare_image.load_images(data_type="test", image_type="image",
-                                                     classification=self._classification,
-                                                     dataset=self.dataset)
-        self.test_labels = prepare_image.load_images(data_type="test", image_type="label",
-                                                     classification=self._classification,
-                                                     dataset=self.dataset)
-        return
-        if args.cache and not os.path.exists(self.cache_image):
-            utility.create_directory(self.cache_image)
-            self._write_hdf5('train_images', self.train_images)
-            self._write_hdf5('train_labels', self.train_labels)
-            self._write_hdf5('test_images', self.test_images)
-            self._write_hdf5('test_labels', self.test_labels)
-
+    
     def run(self):
         print(self.train_images.shape)
         sgd = SGD(lr=1e-3, decay=1e-4, momentum=0.9, nesterov=True)
@@ -227,18 +143,16 @@ class RetinaModel(object):
                                                 verbose=0, save_best_only=True, mode='auto')
         tb_callback = keras.callbacks.TensorBoard(log_dir='./Graph/{}/'.format(time()), histogram_freq=20,
                                      write_graph=True, write_images=False)
-        # tb_callback.set_model(self.model)
-        # weight_save_callback.set_model(self.model)
-        self.model.compile(optimizer=sgd, loss=sigmoid_cross_entropy_with_logits,
-                            metrics=['accuracy',image_accuracy])
 
-        # self.model.fit(self.train_images[:1, ...], self.train_labels[:1, ...], batch_size=1, epochs=1,
-        #               callbacks=[tb_callback], verbose=1)
+        self.model.compile(optimizer=sgd, loss=sigmoid_cross_entropy_with_logits,
+                            metrics=['accuracy', image_accuracy])
+
         self.model.fit(self.train_images, self.train_labels, batch_size=5, epochs=2000,
                         callbacks=[tb_callback], validation_split=0.05, verbose=1)
 
-        self.model.save_weights(os.path.join('cache', 'keras_crop_model_weights_4class_reg_{}.h5'.format(self.activation)))
-
+        self.model.save_weights(os.path.join('cache', 
+                                             'keras_crop_model_weights_4class_reg_{}.h5'.format(self.activation)))
+    
     def predict(self):
         test_predict = self.model.predict(self.test_images, batch_size=10)
         print(test_predict[0])
@@ -247,20 +161,14 @@ class RetinaModel(object):
 
 
 if __name__ == '__main__':
-    pylon5 = os.environ["SCRATCH"] if os.environ.get("SCRATCH", None) else "."
-    pylon5_cache = os.path.join(pylon5, 'cache')
     args = parse_args()
     rm = RetinaModel(classification=args.classification, dataset=args.dataset,
-                     reload=args.reload, activation=args.activation)
+                      reload=args.reload, activation=args.activation, cache=args.cache)
     rm.create_model()
     rm.set_weights()
     rm.get_data()
     print(rm.test_labels.shape)
     print(rm.train_images.shape)
-    # print(rm.model.layers[1].get_weights())
-    # print(rm.model.layers[1].output_shape)
-    # plot_model(rm.model,"model.png")
     # rm.run()
     # rm.predict()
-    # print(rm.model.summary())
     K.clear_session()
